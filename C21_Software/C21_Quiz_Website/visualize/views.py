1ff774133745cedea60ca8d7c4730fbc9db46f2e
DangLamTung <danglamtung199@gmail.com>
2020-10-14 17:34:04 +0700

    add last thing to do

--- C21_Software/C21_Quiz_Website/visualize/views.py
+++ C21_Software/C21_Quiz_Website/visualize/views.py
@@ -11,7 +11,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response 
 import sqlite3
 
-conn = sqlite3.connect('db.sqlite3')
+
 
 class HomeView(View): 
     def get(self, request, *args, **kwargs): 
@@ -41,13 +41,41 @@ class ChartData(APIView):
     permission_classes = [] 
     
     def get(self, request, format = None): 
+        conn = sqlite3.connect('db.sqlite3')
+        cur = conn.cursor()
+        cur.execute("SELECT username FROM auth_user")
+        labels = cur.fetchall()
 
-        # cur = conn.cursor()
-        # cur.execute("SELECT user FROM auth_user")
-        # labels = cur.fetchall()
+        cur.execute("SELECT id FROM auth_user")
+        user_id_bug = cur.fetchall()
+
+        cur.execute("SELECT user_id FROM quiz_progress")
+        user_id = cur.fetchall()
+
+        cur.execute("SELECT current_score FROM quiz_sitting")
+        user_score = cur.fetchall()
+
+        cur.execute("SELECT user_id FROM quiz_sitting")
+        user_id_score = cur.fetchall()
+        real_score = []
         
-        # cur.execute("SELECT user FROM auth_user")
-        # labels = cur.fetchall()
+        for i in user_id:
+            max = -1
+            for (count,j) in enumerate(user_score):
+                if (i == user_id_score[count]):
+                    
+                    if(max < j[0]):
+                        max = j[0]
+            real_score.append(max)
+        real_labels = []
+        for i in user_id:
+            real_labels.append(labels[i[0] - 1])
+        print(user_id_bug)
+        print(labels)
+        print(user_id)
+        print(user_score)
+        print(user_id_score)
+        print(real_score)
         labels = [ 
             'January', 
             'February',  
@@ -60,8 +88,8 @@ class ChartData(APIView):
         chartLabel = "my data"
         chartdata = [0, 10, 5, 2, 20, 30, 45] 
         data ={ 
-                     "labels":labels, 
+                     "labels":real_labels, 
                      "chartLabel":chartLabel, 
-                     "chartdata":chartdata, 
+                     "chartdata":real_score, 
              } 
         return Response(data) 
