72960fbf2c6ea8c93e504d9235f8332ddc37a403
DangLamTung <danglamtung199@gmail.com>
2020-10-23 23:09:51 +0700

    add answer for the first exercise

--- C21_Software/C21_C_basic/Bai2.cpp
+++ C21_Software/C21_C_basic/Bai2.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-float vector_mul(float *a, float*b, int size){
+float vector_mul(float a[], float b [], int size){
 	float temp = 0;
 	for(int i = 0; i<size; i++){
 		temp += a[i]*b[i]; // This problem is not hard too, nothing too special
@@ -10,9 +10,9 @@ int main(){
 	float a[3] = {3.5,5,6};
 	float b[3] = {1,2,3.4};
 	
-	float* v1 = a;
-	float* v2 = a;
-	float result = vector_mul(v1, v2, 3);
+//	float* v1 = a;
+//	float* v2 = a;
+	float result = vector_mul(a, b, 3);
 	printf("%f", result);
 	return 0;
 }
--- C21_Software/C21_C_basic/Bai3.2.cpp
+++ C21_Software/C21_C_basic/Bai3.2.cpp
@@ -6,33 +6,33 @@
 // To multipy 2 matrix, the row of the first matrix must equal to the column of 
 // the second matrix, so we use m to represent that number, n to represent first matrix
 // column and l for second matrix row
-float ** matrix_mul(float a[][3], float b[][2], int m, int n, int l){
+void matrix_mul(float a[][3], float b[][2], float c[][2], int m, int n, int l){
 	float ** temp_data = (float **) calloc(n,(n)*sizeof(float *)); // This is a pointer to the heap section of memory, I use calloc to allocate n*l data for the 
 	                                                               // function to return
     for(int i = 0; i < n; i++){
     	temp_data[i] = (float *) calloc(l,(l)*sizeof(float));
     	for(int j = 0; j < l; j++){
     		for(int k = 0; k < m; k++){
-    			temp_data[i][j] += a[i][k]*b[k][j];              // Matrix multipication
+    			c[i][j] += a[i][k]*b[k][j];              // Matrix multipication
     		}
     	}
 	}
-    return temp_data;
+
 }
 int main(){
 	float a[2][3] = {{1,1,1}, {2,2,2}};
 	float b[3][2] = {{4,4}, {5,5}, {6,6}};
-    float **result = matrix_mul(a, b, 3, 2, 2);
+	float c[2][2];
+    matrix_mul(a, b,c, 3, 2, 2);
     
 //	printf("%f", result);
     
         for(int i = 0; i < 2; i++){
         	for(int j = 0; j < 2; j++){
-        		printf("%f", result[i][j]);
+        		printf("%f",c[i][j]);
 			}
             
 	  printf("\n");
 }
-	free(result);
 	return 0;
 }
